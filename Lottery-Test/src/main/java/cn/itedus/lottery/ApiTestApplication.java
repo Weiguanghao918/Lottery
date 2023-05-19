394c09b6ceeaa8bebc9740cf98540ad94988e652
weiguanghao <1525658286@qq.com>
2023-05-19 17:40:05 +0800

    wgh,fix：修改测试类

---
+++ Lottery-Test/src/main/java/cn/itedus/lottery/ApiTestApplication.java
@@ -0,0 +1,18 @@
+package cn.itedus.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Guanghao Wei
+ * @date: 2023-05-19 17:32
+ * @description:
+ */
+@SpringBootApplication
+@Configuration
+public class ApiTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestApplication.class, args);
+    }
+}
