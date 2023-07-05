1b6603713c0953957fb9ffeed995cc9d20d795a6
weiguanghao <1525658286@qq.com>
2023-07-05 10:41:25 +0800

    wgh,feat: 使用 Nacos 注册中心的方式替换原有Dubbo的直连方式和广播模式。

--- lottery-interfaces/src/main/java/cn/itedus/lottery/LotteryApplication.java
+++ lottery-interfaces/src/main/java/cn/itedus/lottery/LotteryApplication.java
@@ -1,6 +1,7 @@
 package cn.itedus.lottery;
 
 
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Configurable
 @EnableDubbo
+@EnableNacosDiscovery
 public class LotteryApplication {
     public static void main(String[] args) {
         SpringApplication.run(LotteryApplication.class, args);
